cc7c86c1b09e51c263f9e49e719a3e92aee7f4c3
andrewcgaitskellhs2 <36811435+andrewcgaitskellhs2@users.noreply.github.com>
2018-03-07 16:18:03 +0000

    Update list-of-sheets.js

--- list-of-sheets/list-of-sheets.js
+++ list-of-sheets/list-of-sheets.js
@@ -5,8 +5,9 @@ module.exports = function(RED) {
 		RED.nodes.createNode(this,config);
 		var node = this;
 		node.on('input', function(msg) {
+			var msgfile = msg.payload;
 			msg.payload = msg.payload.SheetNames;
-			node.send(msg);
+			node.send(msgfile,msg);
 		});
 	}
 	RED.nodes.registerType("list-of-sheets",ListOfSheetsNode);
