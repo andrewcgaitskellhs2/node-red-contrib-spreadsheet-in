f36c2d7bf8cf05aa21f2ed33f4b4cc9fe95b88af
andrewcgaitskellhs2 <36811435+andrewcgaitskellhs2@users.noreply.github.com>
2018-03-07 14:55:36 +0000

    Create list-of-sheets.js

---
+++ list-of-sheets/list-of-sheets.js
@@ -0,0 +1,13 @@
+module.exports = function(RED) {
+	const util = require('util');
+
+	function ListOfSheetsNode(config) {
+		RED.nodes.createNode(this,config);
+		var node = this;
+		node.on('input', function(msg) {
+			msg.payload = msg.payload.Sheets;
+			node.send(msg);
+		});
+	}
+	RED.nodes.registerType("list-of-sheets",ListOfSheetsNode);
+}
